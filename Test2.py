2c3ab03a6ca1bf9400b6e479e135f3f9757b958b
DaoudaOYM <daoudaorouyorou@gmail.com>
2024-12-10 16:56:44 +0100

    Add files via upload
    Des fichiers pour les tests python pendant mes débuts avec ce magnifique langage

---
+++ Test2.py
@@ -0,0 +1,13 @@
+chaine = "Bonjour"
+nombre_entier = 234
+nombre_decimal = 15.8
+bouleen = True
+print (chaine)
+print(nombre_entier)
+print(nombre_decimal)
+print(bouleen)
+
+print("Je m'appelle Daouda")
+print('Elle s\'appelle Ella')
+print('Il disait: "La vie est un long fleuve tranquille." J\'en suis fier')
+print("Je dis souvent: \"La vie est belle\"")
